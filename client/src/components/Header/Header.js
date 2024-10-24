812d0717bd2d80e54801a725063900489f5a1b20
DAGHHHH <jabrane.jelah@yahoo.com>
2024-10-24 20:42:22 +0100

    l

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,44 @@
 import { LogoutOutlined } from "@ant-design/icons";
 import React, { useState } from "react";
-import { Button, Drawer, Flex, Radio, Space } from 'antd';
+import { Button, Drawer, Flex, Radio, Space, Menu } from 'antd';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+
 
 
 export const Header = () => {
   const [open, setOpen] = useState(false);
   const [placement, setPlacement] = useState('left');
+
+  const items = [
+    {
+      label: 'Navigation Three - Submenu',
+      key: 'SubMenu',
+      icon: <SettingOutlined />,
+      children: [
+        {
+          type: 'group',
+          label: 'Item 1',
+          children: [
+            {
+              label: 'Option 1',
+              key: 'setting:1',
+            },
+            {
+              label: 'Option 2',
+              key: 'setting:2',
+            },
+          ],
+        },
+
+      ],
+    },
+  ];
+
+  const [current, setCurrent] = useState('mail');
+
+  const onClick = (e) => {
+    setCurrent(e.key);
+  };
 
   const showDrawer = () => {
     setOpen(true);
@@ -24,82 +57,101 @@ export const Header = () => {
     <div
       // style={{ borderBottom: "solid 1px #EBECF0 ", padding: "0 10px" }}
       className="header"
+      style={{ backgroundColor: '#0078d4', padding: '10px' }}
     >
-      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", width: '95%', margin: 'auto' }} className="container">
+      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }} className="container">
 
-
+        {/* left items */}
         <div className="right-nav-items">
-          <div className="items-container">
-            <div>
-                <Space>
-                  {/* <Radio.Group value={placement} onChange={onChange}>
-                    <Radio value="top">top</Radio>
-                    <Radio value="right">right</Radio>
-                    <Radio value="bottom">bottom</Radio>
-                    <Radio value="left">left</Radio>
-                  </Radio.Group> */}
-                  <div className="drawrer-opner">
-                    <Button type="" onClick={showDrawer} className="hamburger-button">
-                      <span className="dash"></span>
-                      <span className="dash"></span>
-                      <span className="dash"></span>
-                    </Button>
-
-                  </div>
-
-                </Space>
-                <Drawer
-                  // title="Basic Drawer"
-                  placement={placement}
-                  closable={false}
-                  onClose={onClose}
-                  open={open}
-                  key={placement}
-                >
-                  <Button type="" onClick={CloseDrawrer} className="hamburger-button">
-                    <span className="dash"></span>
-                    <span className="dash"></span>
-                    <span className="dash"></span>
-                  </Button>
-
-                  <div className="sidebar-items">
-                    <ul style={{ padding: '0px' }}>
-                      <li style={{ listStyle: 'none', padding: '8px', alignItems: 'center', gap: "30px" }}>
-                        <span>Icon</span>
-                        <button style={{ backgroundColor: "transparent", border: "none" }} >item & & 1</button>
-                      </li>
-                      <li style={{ listStyle: 'none', padding: '8px', alignItems: 'center', gap: "30px" }}>
-                        <span>Icon</span>
-                        <button style={{ backgroundColor: "transparent", border: "none" }} >item & & 1</button>
-                      </li>
-                      <li style={{ listStyle: 'none', padding: '8px', alignItems: 'center', gap: "30px" }}>
-                        <span>Icon</span>
-                        <button style={{ backgroundColor: "transparent", border: "none" }} >item & & 1</button>
-                      </li>
-                      <li style={{ listStyle: 'none', padding: '8px', alignItems: 'center', gap: "30px" }}>
-                        <span>Icon</span>
-                        <button style={{ backgroundColor: "transparent", border: "none" }} >item & & 1</button>
-                      </li>
-                      <li style={{ listStyle: 'none', padding: '8px', alignItems: 'center', gap: "30px" }}>
-                        <span>Icon</span>
-                        <button style={{ backgroundColor: "transparent", border: "none" }} >item & & 1</button>
-                      </li>
-
-                    </ul>
-
-                  </div>
-
-
-                </Drawer>
-
+          <div style={{ display: 'flex', alignItems: 'center', gap: '20px' }} className="items-container">
+            <div className="drawrer-opner">
+              <button style={{ cursor: "pointer" }} type="" onClick={showDrawer} className="hamburger-button">
+                <span className="dash"></span>
+                <span className="dash"></span>
+                <span className="dash"></span>
+              </button>
 
             </div>
+            <div className="logo">
+              <span className="" style={{ color: 'whitesmoke' }} >e-Recrutement</span>
+            </div>
+
 
           </div>
 
 
 
+
+
+
+
         </div>
+        {/* Right items */}
+        <div style={{ display: 'flex', alignItems: 'center' }} className="items-container">
+
+          <Menu
+            onClick={onClick}
+            selectedKeys={[current]}
+            triggerSubMenuAction="hover" // Ensures the submenu opens on hover
+            style={{
+              display: 'flex',
+              backgroundColor: 'transparent',  // Set background to transparent
+              color :"whitesmoke"
+            }}
+            items={items}
+          />
+
+
+        </div>
+      </div>
+
+      <div>
+
+        <Drawer
+          // title="Basic Drawer"
+          placement={placement}
+          closable={false}
+          onClose={onClose}
+          open={open}
+          key={placement}
+        >
+          <Button type="" onClick={CloseDrawrer} className="hamburger-button">
+            <span className="dash"></span>
+            <span className="dash"></span>
+            <span className="dash"></span>
+          </Button>
+
+          <div className="sidebar-items">
+            <ul style={{ padding: '0px' }}>
+              <li style={{ listStyle: 'none', padding: '8px', alignItems: 'center', gap: "30px" }}>
+                <span>Icon</span>
+                <button style={{ backgroundColor: "transparent", border: "none" }} >item & & 1</button>
+              </li>
+              <li style={{ listStyle: 'none', padding: '8px', alignItems: 'center', gap: "30px" }}>
+                <span>Icon</span>
+                <button style={{ backgroundColor: "transparent", border: "none" }} >item & & 1</button>
+              </li>
+              <li style={{ listStyle: 'none', padding: '8px', alignItems: 'center', gap: "30px" }}>
+                <span>Icon</span>
+                <button style={{ backgroundColor: "transparent", border: "none" }} >item & & 1</button>
+              </li>
+              <li style={{ listStyle: 'none', padding: '8px', alignItems: 'center', gap: "30px" }}>
+                <span>Icon</span>
+                <button style={{ backgroundColor: "transparent", border: "none" }} >item & & 1</button>
+              </li>
+              <li style={{ listStyle: 'none', padding: '8px', alignItems: 'center', gap: "30px" }}>
+                <span>Icon</span>
+                <button style={{ backgroundColor: "transparent", border: "none" }} >item & & 1</button>
+              </li>
+
+            </ul>
+
+          </div>
+
+
+        </Drawer>
+
+
       </div>
     </div>
   );
